72654b48eb2f4d7a0c9ed6e0d69636af6c8355f6
Usman Kiyani <uskiyani@gmail.com>
2020-06-02 09:24:33 +0200

    Header + Footer dynamic links, Loading gif, Area card filter, PHP function file exposing Menu rest-api

--- script.js
+++ script.js
@@ -7,3 +7,74 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+
+// AREA-FILTER
+function areaFilter(clickedArea) {
+        
+    let area;
+    if(clickedArea.includes('alle')) {
+        area = "";
+    } else {
+        area = clickedArea;
+    }
+
+var allAreas = document.querySelectorAll(".kalender");
+allAreas.forEach(function(a) {
+        let aClass = a.className;
+        a.style.display = "none"
+        if(aClass.includes(area) ){
+            a.style.display = ""
+        }
+    });
+}
+
+async function getFooterData(){
+    let res = await fetch('http://orangeduck.dk/Naiha/wp-json/wp/v2/footer_section')
+    footerData = await res.json();
+
+    footerData.map( f => {
+        document.querySelector(".kontakt-tlf").innerHTML = (f.telefon);
+        document.querySelector(".kontakt-mail").innerHTML = (f.mail);
+    })
+} 
+
+getFooterData();
+
+
+async function getMenuLinks() {
+    let res = await fetch('http://orangeduck.dk/Naiha/wp-json/wp/v2/menu');
+    menuItem = await res.json();
+
+    let menuLinks = document.querySelector(".menu-links");
+    let footerLinks = document.querySelector(".footer-links")
+
+    // Set Header Links
+    menuItem.map( m => { 
+
+        var li = document.createElement("LI");
+        var a = document.createElement("a");
+        a.href = m.url;
+        a.innerText = m.title;
+        li.appendChild(a);
+        
+      
+        document.querySelector(".menu-links").appendChild(a);
+        menuLinks.appendChild(a);
+
+    })
+
+    // Set Footer Links
+    menuItem.map( m => { 
+        var li = document.createElement("LI");
+        var a = document.createElement("a");
+
+        a.href = m.url;
+        a.innerText = m.title;
+
+        li.appendChild(a);
+        footerLinks.appendChild(li);
+        })
+}
+
+getMenuLinks();
